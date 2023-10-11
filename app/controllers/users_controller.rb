1476708214573c852f100f7192ab65e1e499f133
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-11 15:19:52 +0000

    finished user routes and pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,9 @@ class UsersController < ApplicationController
     
     the_user.username = params.fetch("query_username")
     the_user.save
-    redirect_to("/users/#{user.username}")
+    
+    new_username = the_user.username
+    redirect_to("/users/#{new_username}")
   end
 
 end
